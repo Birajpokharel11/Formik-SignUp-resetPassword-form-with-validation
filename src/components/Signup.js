6b5a64b6653af13b6069a32b52e7edebb6fe1223
Biraj Pokharel <buraj.sainik@gmail.com>
2021-08-15 00:20:03 +0545

    Signup form updated

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,113 +1,138 @@
+/* eslint-disable no-const-assign */
+/* eslint-disable no-unused-expressions */
 import React from 'react';
-import clsx from 'clsx';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/styles';
-import {
-  Card,
-  CardHeader,
-  CardContent,
-  Divider,
-  Grid,
-  Button,
-  TextField,
-  Container
-} from '@material-ui/core';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
-const useStyles = makeStyles(() => ({
-  root: {}
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
 }));
 const validationSchema = yup.object({
   name: yup.string('Enter your Name').required('Name is required'),
   email: yup
     .string('Enter your email')
     .email('Enter a valid email')
-    .required('Email is required')
+    .required('Email is required'),
+  password: yup
+    .string('Enter your password')
+    .min(8, 'Password should be of minimum 8 characters length')
+    .required('Password is required')
 });
-const AccountDetails = (props) => {
-  const { className, user, UpdateUsername, ...rest } = props;
 
+function SignUp() {
   const classes = useStyles();
-
   const formik = useFormik({
     initialValues: {
-      name: user?.name,
-      email: user?.email
+      name: '',
+      email: '',
+      password: ''
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
-      const { name, email } = values;
+      const { name, email, password } = values;
       alert(JSON.stringify(values, null, 2));
-      console.log(email, name);
     }
   });
-  return (
-    <Container>
-      <Card {...rest} className={clsx(classes.root, className)}>
-        <form autoComplete="off" onSubmit={formik.handleSubmit}>
-          <CardHeader
-            subheader="The information can be edited"
-            title="Profile"
-          />
-          <Divider />
-          <CardContent>
-            <Grid container spacing={3}>
-              <Grid item md={6} xs={12}>
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="name"
-                  label="name"
-                  name="name"
-                  autoComplete="name"
-                  autoFocus
-                  value={formik.values.name}
-                  onChange={formik.handleChange}
-                  error={formik.touched.name && Boolean(formik.errors.name)}
-                  helperText={formik.touched.name && formik.errors.name}
-                />
-              </Grid>
 
-              <Grid item md={6} xs={12}>
-                <TextField
-                  fvariant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  autoFocus
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  error={formik.touched.email && Boolean(formik.errors.email)}
-                  helperText={formik.touched.email && formik.errors.email}
-                />
-              </Grid>
+  return (
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign Up
+        </Typography>
+        <form onSubmit={formik.handleSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="name"
+            name="name"
+            autoComplete="name"
+            autoFocus
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            error={formik.touched.name && Boolean(formik.errors.name)}
+            helperText={formik.touched.name && formik.errors.name}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+          />{' '}
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            margin="normal"
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+          />
+          <Button
+            color="primary"
+            variant="contained"
+            fullWidth
+            type="submit"
+            className={classes.submit}
+          >
+            Submit
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link to="/login" variant="body2">
+                {'Already have account? Sign Up'}
+              </Link>
             </Grid>
-            <Divider />
-            <br />
-            <Button
-              color="primary"
-              variant="contained"
-              fullWidth
-              type="submit"
-              className={classes.submit}
-            >
-              Save details
-            </Button>
-          </CardContent>
+          </Grid>
         </form>
-      </Card>
+      </div>
     </Container>
   );
-};
+}
 
-AccountDetails.propTypes = {
-  className: PropTypes.string
-};
-
-export default AccountDetails;
+export default SignUp;
